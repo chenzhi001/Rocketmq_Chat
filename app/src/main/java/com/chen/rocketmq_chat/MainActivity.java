ec73fd07e9cc37a7ea141592eeb8d976b6684eb8
chenzhi <1062029783@qq.com>
2018-07-06 18:23:56 +0800

    整理报错

--- app/src/main/java/com/chen/rocketmq_chat/MainActivity.java
+++ app/src/main/java/com/chen/rocketmq_chat/MainActivity.java
@@ -1,5 +1,4 @@
 package com.chen.rocketmq_chat;
-
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
--- app/src/main/java/com/chen/rocketmq_chat/base/BaseMvpAppCompatActivity.java
+++ app/src/main/java/com/chen/rocketmq_chat/base/BaseMvpAppCompatActivity.java
@@ -1,8 +1,10 @@
 package com.chen.rocketmq_chat.base;
+
 import com.chen.rocketmq_chat.presenter.MvpBasePresenter;
+
 /**
  * @author:Orange Created by 2018/7/6.
  */
-public class BaseMvpAppCompatActivity<P extends MvpBasePresenter> {
+public class BaseMvpAppCompatActivity<P extends MvpBasePresenter, V extends MvpView> {
 
 }
